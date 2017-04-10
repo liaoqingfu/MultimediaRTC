7c87662181d4660992eacb0357a0ff0a7dd2957f
gaotianpeng <gaotianpeng@whaty.com>
2017-04-10 10:05:26 +0800

    Read Camera Framework test

--- src/MultimediaRTC.cpp
+++ src/MultimediaRTC.cpp
@@ -1,174 +1,142 @@
-/*
-	* This is the simplest program based on FFmpeg API. It can show your the following
-	* informations about FFmpeg library:
-	* Protocol:  Protocols supported by FFmpeg.
-	* AVFormat:  Container format supported by FFmpeg.
-	* AVCodec:   Encoder/Decoder supported by FFmpeg.
-	* AVFilter:  Filters supported by FFmpeg.
-	* Configure: configure information of FFmpeg.
-*/
-
 #include <stdio.h>  
 
 #define __STDC_CONSTANT_MACROS  
 
-//Windows  
 extern "C"
 {
+#include "libavdevice/avdevice.h"
 #include "libavcodec/avcodec.h"  
 #include "libavformat/avformat.h"  
 #include "libavfilter/avfilter.h"  
 };
 
 
-//FIX  
-struct URLProtocol;
-/*
-	* Protocol Support Information
-*/
-char* urlprotocolinfo() 
-{
-	char *info = (char *)malloc(40000);
-	memset(info, 0, 40000);
-
-	av_register_all();
-
-	struct URLProtocol *pup = NULL;
-	//Input  
-	struct URLProtocol **p_temp = &pup;
-	avio_enum_protocols((void **)p_temp, 0);
-	while ((*p_temp) != NULL) {
-		sprintf(info, "%s[In ][%10s]\n", info, avio_enum_protocols((void **)p_temp, 0));
-	}
-	pup = NULL;
-	//Output  
-	avio_enum_protocols((void **)p_temp, 1);
-	while ((*p_temp) != NULL) {
-		sprintf(info, "%s[Out][%10s]\n", info, avio_enum_protocols((void **)p_temp, 1));
-	}
-
-	return info;
-}
-
-/*
-	* AVFormat Support Information
-*/
-char* avformatinfo() 
-{
-	char *info = (char *)malloc(40000);
-	memset(info, 0, 40000);
-
-	av_register_all();
-
-	AVInputFormat *if_temp = av_iformat_next(NULL);
-	AVOutputFormat *of_temp = av_oformat_next(NULL);
-	//Input  
-	while (if_temp != NULL) {
-		sprintf(info, "%s[In ] %10s\n", info, if_temp->name);
-		if_temp = if_temp->next;
-	}
-	//Output  
-	while (of_temp != NULL) {
-		sprintf(info, "%s[Out] %10s\n", info, of_temp->name);
-		of_temp = of_temp->next;
-	}
-	return info;
-}
-
-/*
-	* AVCodec Support Information
-*/
-char* avcodecinfo()
-{
-	char *info = (char *)malloc(40000);
-	memset(info, 0, 40000);
-
-	av_register_all();
-
-	AVCodec *c_temp = av_codec_next(NULL);
-
-	while (c_temp != NULL) {
-		if (c_temp->decode != NULL) {
-			sprintf(info, "%s[Dec]", info);
-		}
-		else {
-			sprintf(info, "%s[Enc]", info);
-		}
-		switch (c_temp->type) {
-		case AVMEDIA_TYPE_VIDEO:
-			sprintf(info, "%s[Video]", info);
-			break;
-		case AVMEDIA_TYPE_AUDIO:
-			sprintf(info, "%s[Audio]", info);
-			break;
-		default:
-			sprintf(info, "%s[Other]", info);
-			break;
-		}
-
-		sprintf(info, "%s %10s\n", info, c_temp->name);
-
-		c_temp = c_temp->next;
-	}
-	return info;
-}
-
-/*
-	* AVFilter Support Information
-*/
-char* avfilterinfo()
-{
-	char *info = (char *)malloc(40000);
-	memset(info, 0, 40000);
-
-	avfilter_register_all();
-
-	AVFilter *f_temp = (AVFilter *)avfilter_next(NULL);
-
-	while (f_temp != NULL) {
-		sprintf(info, "%s[%15s]\n", info, f_temp->name);
-		f_temp = f_temp->next;
-	}
-	return info;
-}
-
-/*
-	* Configuration Information
-*/
-char* configurationinfo()
-{
-	char *info = (char *)malloc(40000);
-	memset(info, 0, 40000);
-
-	av_register_all();
-
-	sprintf(info, "%s\n", avcodec_configuration());
-
-	return info;
-}
-
 int main(int argc, char* argv[])
 {
-	char *infostr = NULL;
-	infostr = configurationinfo();
-	printf("\n<<Configuration>>\n%s", infostr);
-	free(infostr);
+	/*
+		Step 1: Do some initialize work.
+	*/
+	// Register device. 
+	avdevice_register_all();
+	// Initialize libavformat and register all the muxers, demuxers and protocols.
+	av_register_all(); 
+	//Do global initialization of network components.
+	avformat_network_init(); 
 
-	infostr = urlprotocolinfo();
-	printf("\n<<URLProtocol>>\n%s", infostr);
-	free(infostr);
+	/*
+		Step 2: Open the device.
+	*/
+	// Allocate an AVFormatContext.
+	AVFormatContext* pFormatCtx = NULL; 
+	pFormatCtx = avformat_alloc_context(); 
 
-	infostr = avformatinfo();
-	printf("\n<<AVFormat>>\n%s", infostr);
-	free(infostr);
+	AVDictionary* options = NULL; 
+	av_dict_set(&options, "list_devices", "true", 0);
+	AVInputFormat* pInputFormat = NULL; 
+	pInputFormat = av_find_input_format("dshow"); 
+	printf("==============Device Info==============\n"); 
+	//avformat_open_input(&pFormatContext, "Video=Logitech HD Webcam C310",pInputFormat, &options); 
+	avformat_open_input(&pFormatCtx, "video=dummy", pInputFormat, &options); 
+	printf("=======================================\n"); 
 
-	infostr = avcodecinfo();
-	printf("\n<<AVCodec>>\n%s", infostr);
-	free(infostr);
+	/*
+		Step 3: Get stream information and get the codec information.
+	*/
+	//Read packets of a media file to get stream information. 
+	if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
+	{
+		printf("Can't find stream information.\n"); 
+	}
+	int videoIndex = -1; 
+	for (int i = 0; i < pFormatCtx->nb_streams; ++i)
+	{
+		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			videoIndex = i; 
+			break; 
+		}
+	}
+	if (videoIndex == -1)
+	{
+		printf("Can't find a video stream.\n"); 
+		return -1; 
+	}
 
-	infostr = avfilterinfo();
-	printf("\n<<AVFilter>>\n%s", infostr);
-	free(infostr);
+	AVCodecContext* pCodecCtx = NULL; 
+	AVCodec* pCodec = NULL; 
+	pCodecCtx = pFormatCtx->streams[videoIndex]->codec; 
+	pCodec = avcodec_find_decoder(pCodecCtx->codec_id); 
+	if (pCodec == NULL)
+	{
+		printf("Codec not found.\n"); 
+		return -1; 
+	}
+	// Initialize the AVCodecContext to use the given AVCodec
+	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0)
+	{
+		printf("Can't open codec.\n"); 
+		return -1; 
+	}
+
+	/*
+		Step 4: Get raw frames video data of the video captured from the camera 
+		Then write the raw frames video data to a YUV420 type local file. 
+	*/
+	AVFrame* pFrame = NULL; 
+	pFrame = av_frame_alloc();
+	AVFrame* pFrameYUV = NULL; 
+	pFrameYUV = av_frame_alloc(); 
+
+	AVPacket* pPacket = av_packet_alloc(); 
+
+	FILE* fpYUV = fopen("output.yuv", "wb+"); 
+
+	int got_picture = 0; 
+
+	for (;;)
+	{
+		if (av_read_frame(pFormatCtx, pPacket) >= 0)
+		{
+			if (pPacket->stream_index == videoIndex)
+			{
+				if (avcodec_decode_video2(pCodecCtx, pFrameYUV, &got_picture, pPacket) < 0)
+				{
+					printf("Decode Error.\n"); 
+					return -1; 
+				}
+			}
+
+			int y_size = pCodecCtx->width*pCodecCtx->height;
+			fwrite(pFrameYUV->data[0], 1, y_size, fpYUV);	   //Y   
+			fwrite(pFrameYUV->data[1], 1, y_size / 4, fpYUV);  //U  
+			fwrite(pFrameYUV->data[2], 1, y_size / 4, fpYUV);  //V  
+
+		}
+		else 
+		{
+			break; 
+		}
+	}
+
+	/*
+		Step 5: Read the raw frames video data(YUV420), encode(H.264), formated(mp4) 
+	*/
+	   
+	                                                   
+
+
+	/*
+		Step 6: Free the resource.
+	*/
+
+	fclose(fpYUV); 
+	av_packet_free(&pPacket); 
+	av_frame_free(&pFrame); 
+	avcodec_free_context(&pCodecCtx); 
+	avcodec_close(pCodecCtx);
+	avformat_close_input(&pFormatCtx); 
+	avformat_free_context(pFormatCtx); 
+
 
 	return 0;
 }
