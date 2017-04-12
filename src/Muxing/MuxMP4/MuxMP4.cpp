7fb4e6ada3adac2ea19d55cc116423dce10ebb7b
gaotianpeng <gaotianpeng@whaty.com>
2017-04-12 15:09:23 +0800

    Change the code style of the project MuxMP4

--- src/Muxing/MuxMP4/MuxMP4.cpp
+++ src/Muxing/MuxMP4/MuxMP4.cpp
@@ -33,23 +33,23 @@ int main(int argc, char* argv[])
 	int ret = 1; 
 	if ((ret = avformat_open_input(&pInFmtCtxV, pInFileNameV, 0, 0)) < 0) 
 	{
-		printf( "Can't open input file.");
+		printf( "Can't open input file.\n");
 		goto end;
 	}
 	if ((ret = avformat_find_stream_info(pInFmtCtxV, 0)) < 0) 
 	{
-		printf( "Failed to retrieve input stream information.");
+		printf( "Failed to retrieve input stream information.\n");
 		goto end;
 	}
 
 	if ((ret = avformat_open_input(&pInFmtCtxA, pInFileNameA, 0, 0)) < 0) 
 	{
-		printf( "Can't open input file.");
+		printf( "Can't open input file.\n");
 		goto end;
 	}
 	if ((ret = avformat_find_stream_info(pInFmtCtxA, 0)) < 0) 
 	{
-		printf( "Failed to retrieve input stream information.");
+		printf( "Failed to retrieve input stream information.\n");
 		goto end;
 	}
 
@@ -86,7 +86,7 @@ int main(int argc, char* argv[])
 				ret = AVERROR_UNKNOWN;
 				goto end;
 			}
-			videoIndexOut=pOutStream->index;
+			videoIndexOut = pOutStream->index;
 			//Copy the settings of AVCodecContext
 			if (avcodec_copy_context(pOutStream->codec, pInStream->codec) < 0)
 			{
@@ -106,27 +106,28 @@ int main(int argc, char* argv[])
 	for (int i = 0; i < pInFmtCtxA->nb_streams; i++) 
 	{
 		//Create output AVStream according to input AVStream
-		if(pInFmtCtxA->streams[i]->codec->codec_type==AVMEDIA_TYPE_AUDIO){
-			AVStream *in_stream = pInFmtCtxA->streams[i];
-			AVStream *out_stream = avformat_new_stream(pOutFmtCtx, in_stream->codec->codec);
-			audioIndexA=i;
-			if (!out_stream) 
+		if(pInFmtCtxA->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO)
+		{
+			AVStream *pInStream = pInFmtCtxA->streams[i];
+			AVStream *pOutStream = avformat_new_stream(pOutFmtCtx, pInStream->codec->codec);
+			audioIndexA = i;
+			if (!pOutStream) 
 			{
 				printf( "Failed allocating output stream\n");
 				ret = AVERROR_UNKNOWN;
 				goto end;
 			}
-			audioIndexOut=out_stream->index;
+			audioIndexOut = pOutStream->index;
 			//Copy the settings of AVCodecContext
-			if (avcodec_copy_context(out_stream->codec, in_stream->codec) < 0)
+			if (avcodec_copy_context(pOutStream->codec, pInStream->codec) < 0)
 			{
 				printf( "Failed to copy context from input to output stream codec context\n");
 				goto end;
 			}
-			out_stream->codec->codec_tag = 0;
+			pOutStream->codec->codec_tag = 0;
 			if (pOutFmtCtx->oformat->flags & AVFMT_GLOBALHEADER)
 			{
-				out_stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
+				pOutStream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
 			}
 			break;
 		}
@@ -140,13 +141,13 @@ int main(int argc, char* argv[])
 		Step 4: Write file header.
 	*/
 	//Open output file
-	AVOutputFormat *ofmt = NULL;
-	ofmt = pOutFmtCtx->oformat;
-	if (!(ofmt->flags & AVFMT_NOFILE))
+	AVOutputFormat *pOutputFmt = NULL;
+	pOutputFmt = pOutFmtCtx->oformat;
+	if (!(pOutputFmt->flags & AVFMT_NOFILE))
 	{
 		if (avio_open(&pOutFmtCtx->pb, pOutFileName, AVIO_FLAG_WRITE) < 0) 
 		{
-			printf( "Could not open output file '%s'", pOutFileName);
+			printf( "Can't open output file '%s'", pOutFileName);
 			goto end;
 		}
 	}
@@ -161,115 +162,117 @@ int main(int argc, char* argv[])
 		Step 5: Get the AVPakcet from A/V stream and write into the output file.
 	*/
 	AVPacket packet;
-	int frame_index = 0;
-	int64_t cur_pts_v = 0, cur_pts_a = 0;
+	int frameIndex = 0;
+	int64_t curPtsV = 0, curPtsA = 0;
 
 	AVBitStreamFilterContext* h264bsfc =  av_bitstream_filter_init("h264_mp4toannexb"); 
 	AVBitStreamFilterContext* aacbsfc =  av_bitstream_filter_init("aac_adtstoasc"); 
 
 	while (1)
 	{
-		AVFormatContext *ifmt_ctx;
-		int stream_index = 0;
-		AVStream *in_stream = NULL, *out_stream = NULL;
+		AVFormatContext *pInputFromat = NULL;
+		int streamIndex = 0;
+		AVStream *pInStream = NULL, *pOutStream = NULL;
 
 		//Get an AVPacket
-		if(av_compare_ts(cur_pts_v,pInFmtCtxV->streams[videoIndexV]->time_base,
-			cur_pts_a,pInFmtCtxA->streams[audioIndexA]->time_base) <= 0)
+		if (av_compare_ts(curPtsV, pInFmtCtxV->streams[videoIndexV]->time_base,
+			curPtsA, pInFmtCtxA->streams[audioIndexA]->time_base) <= 0)
 		{
-			ifmt_ctx=pInFmtCtxV;
-			stream_index=videoIndexOut;
+			pInputFromat = pInFmtCtxV;
+			streamIndex = videoIndexOut;
 
-			if(av_read_frame(ifmt_ctx, &packet) >= 0)
+			if (av_read_frame(pInputFromat, &packet) >= 0)
 			{
-				do{
-					in_stream  = ifmt_ctx->streams[packet.stream_index];
-					out_stream = pOutFmtCtx->streams[stream_index];
+				do
+				{
+					pInStream  = pInputFromat->streams[packet.stream_index];
+					pOutStream = pOutFmtCtx->streams[streamIndex];
 
-					if(packet.stream_index == videoIndexV)
+					if (packet.stream_index == videoIndexV)
 					{
-						//FIX£ºNo PTS (Example: Raw H.264)
-						//Simple Write PTS
-						if(packet.pts == AV_NOPTS_VALUE)
+						// FIX£ºNo PTS (Example: Raw H.264)
+						// Simple Write PTS
+						if (packet.pts == AV_NOPTS_VALUE)
 						{
-							//Write PTS
-							AVRational time_base1 = in_stream->time_base;
-							//Duration between 2 frames (us)
-							int64_t calc_duration = (double)AV_TIME_BASE / av_q2d(in_stream->r_frame_rate);
-							//Parameters
-							packet.pts = (double)(frame_index*calc_duration) / (double)(av_q2d(time_base1) * AV_TIME_BASE);
+							// Write PTS
+							AVRational time_base1 = pInStream->time_base;
+							// Duration between 2 frames (us)
+							int64_t calc_duration = (double)AV_TIME_BASE / av_q2d(pInStream->r_frame_rate);
+							// Parameters
+							packet.pts = (double)(frameIndex*calc_duration) / (double)(av_q2d(time_base1) * AV_TIME_BASE);
 							packet.dts = packet.pts;
 							packet.duration = (double)calc_duration / (double)(av_q2d(time_base1) * AV_TIME_BASE);
-							frame_index++;
+							frameIndex++;
 						}
 
-						cur_pts_v=packet.pts;
+						curPtsV = packet.pts;
 						break;
 					}
-				} while (av_read_frame(ifmt_ctx, &packet) >= 0);
+				} while (av_read_frame(pInputFromat, &packet) >= 0);
 			}
 			else
 			{
 				break;
 			}
-		}else{
-			ifmt_ctx=pInFmtCtxA;
-			stream_index=audioIndexOut;
-			if(av_read_frame(ifmt_ctx, &packet) >= 0)
+		}
+		else
+		{
+			pInputFromat = pInFmtCtxA;
+			streamIndex = audioIndexOut;
+			if (av_read_frame(pInputFromat, &packet) >= 0)
 			{
-				do{
-					in_stream  = ifmt_ctx->streams[packet.stream_index];
-					out_stream = pOutFmtCtx->streams[stream_index];
+				do
+				{
+					pInStream  = pInputFromat->streams[packet.stream_index];
+					pOutStream = pOutFmtCtx->streams[streamIndex];
 
-					if(packet.stream_index == audioIndexA)
+					if (packet.stream_index == audioIndexA)
 					{
-						//FIX£ºNo PTS
-						//Simple Write PTS
-						if(packet.pts == AV_NOPTS_VALUE)
+						// FIX£ºNo PTS
+						// Simple Write PTS
+						if (packet.pts == AV_NOPTS_VALUE)
 						{
-							//Write PTS
-							AVRational time_base1=in_stream->time_base;
-							//Duration between 2 frames (us)
-							int64_t calc_duration=(double)AV_TIME_BASE / av_q2d(in_stream->r_frame_rate);
-							//Parameters
-							packet.pts = (double)(frame_index*calc_duration) / (double)(av_q2d(time_base1) * AV_TIME_BASE);
+							// Write PTS
+							AVRational time_base1 = pInStream->time_base;
+							// Duration between 2 frames (us)
+							int64_t calc_duration = (double)AV_TIME_BASE / av_q2d(pInStream->r_frame_rate);
+							// Parameters
+							packet.pts = (double)(frameIndex * calc_duration) / (double)(av_q2d(time_base1) * AV_TIME_BASE);
 							packet.dts = packet.pts;
 							packet.duration = (double)calc_duration / (double)(av_q2d(time_base1) * AV_TIME_BASE);
-							frame_index++;
+							frameIndex++;
 						}
-						cur_pts_a = packet.pts;
+						curPtsA = packet.pts;
 						break;
 					}
-				}while(av_read_frame(ifmt_ctx, &packet) >= 0);
+				} while (av_read_frame(pInputFromat, &packet) >= 0);
 			}
 			else
 			{
 				break;
 			}
-
 		}
 
 
-		av_bitstream_filter_filter(h264bsfc, in_stream->codec, NULL, &packet.data,
+		av_bitstream_filter_filter(h264bsfc, pInStream->codec, NULL, &packet.data,
 									&packet.size, packet.data, packet.size, 0);
-		av_bitstream_filter_filter(aacbsfc, out_stream->codec, NULL, &packet.data, 
+		av_bitstream_filter_filter(aacbsfc, pOutStream->codec, NULL, &packet.data, 
 										&packet.size, packet.data, packet.size, 0);
 
-
-		//Convert PTS/DTS
-		packet.pts = av_rescale_q_rnd(packet.pts, in_stream->time_base, 
-									out_stream->time_base,
+		// Convert PTS/DTS
+		packet.pts = av_rescale_q_rnd(packet.pts, pInStream->time_base, 
+									pOutStream->time_base,
 									(AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-		packet.dts = av_rescale_q_rnd(packet.dts, in_stream->time_base,
-									out_stream->time_base, 
+		packet.dts = av_rescale_q_rnd(packet.dts, pInStream->time_base,
+									pOutStream->time_base, 
 									(AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-		packet.duration = av_rescale_q(packet.duration, in_stream->time_base, 
-									out_stream->time_base);
+		packet.duration = av_rescale_q(packet.duration, pInStream->time_base, 
+									pOutStream->time_base);
 		packet.pos = -1;
-		packet.stream_index=stream_index;
+		packet.stream_index = streamIndex;
 
 		printf("Write 1 Packet. size:%5d\tpts:%lld\n", packet.size, packet.pts);
-		//Write
+		// Write
 		if (av_interleaved_write_frame(pOutFmtCtx, &packet) < 0)
 		{
 			printf( "Error muxing packet\n");
@@ -292,7 +295,7 @@ end:
 	avformat_close_input(&pInFmtCtxV);
 	avformat_close_input(&pInFmtCtxA);
 	
-	if (pOutFmtCtx && !(ofmt->flags & AVFMT_NOFILE))
+	if (pOutFmtCtx && !(pOutputFmt->flags & AVFMT_NOFILE))
 	{
 		avio_close(pOutFmtCtx->pb);
 	}
